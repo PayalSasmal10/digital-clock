858d505b0dda5c85fc6a42ae1d36d206717bb8bc
Payal Sasmal <sasmalpayal@gmail.com>
2023-09-14 22:41:13 +0530

    digital clock is created"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [hours, setHours] = useState(new Date().getHours());
+  const [mins, setMins] = useState(new Date().getMinutes());
+  const [seconds, setSeconds] = useState(new Date().getSeconds());
+
+  useEffect(() => {
+    let interval;
+    interval = setInterval(() => {
+      const currentHour = new Date().getHours();
+      const currentMins = new Date().getMinutes();
+      const currentSeconds = new Date().getSeconds();
+      setHours(currentHour);
+      setMins(currentMins);
+      setSeconds(currentSeconds);
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="outer-container">
+        <div className="inner-container">
+          <span>{hours}</span>:<span>{mins}</span>:<span>{seconds}</span>
+        </div>
+      </div>
     </div>
   );
 }
